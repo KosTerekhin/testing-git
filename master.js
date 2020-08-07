09d567cf2c0859086afd137985450232a34656ce
Kos Terekhin <terekhin.konst@gmail.com>
2020-08-07 13:17:44 +0300

    adding multiply

--- master.js
+++ master.js
@@ -4,8 +4,13 @@ class MathFnc {
 	add(a, b) {
 		return a + b;
 	}
+
+	multiply(a, b) {
+		return a * b;
+	}
 }
 
 const calc = new MathFnc();
 
 console.log(calc.add(5, 10));
+console.log(calc.multiply(5, 10));
